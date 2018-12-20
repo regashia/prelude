1a43417b6525717b4256859aad151d5585cf080f
kiyoshi oh <oh@kiyoshinoMacBook-puro.local>
2018-12-20 15:26:29 +0900

    Change C-h to backspace

---
+++ personal/my.el
@@ -0,0 +1,5 @@
+;; Unix style C-h
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+(define-key key-translation-map (kbd "M-h") (kbd "<C-backspace>"))
+(define-key key-translation-map (kbd "DEL") (kbd "C-h"))
+(define-key key-translation-map (kbd "M-DEL") (kbd "M-h"))
