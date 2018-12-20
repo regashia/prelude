554213df3d6913b690b43aab0ec8b257f5575f13
kiyoshi oh <oh@kiyoshinoMacBook-puro.local>
2018-12-20 15:28:04 +0900

    Always fllow symlink

--- personal/my.el
+++ personal/my.el
@@ -3,3 +3,6 @@
 (define-key key-translation-map (kbd "M-h") (kbd "<C-backspace>"))
 (define-key key-translation-map (kbd "DEL") (kbd "C-h"))
 (define-key key-translation-map (kbd "M-DEL") (kbd "M-h"))
+
+;; Always follow symlink
+(setq vc-follow-symlinks t)
